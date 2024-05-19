bcb161239c7d4d15d70e06ed67c9da9ff15fda17
Grigory <weissman1979@gmail.com>
2024-05-19 16:20:43 +0400

    commit

--- src/ShopItemClass.js
+++ src/ShopItemClass.js
@@ -5,19 +5,19 @@ import ShopModel from './models/ShopModel';
 class ShopItemClass extends React.Component {
 
   render() {
-    const item  = ShopModel.items;
+    const { brand, title, description, descriptionFull, price, currency } = this.props.info;
     return (
         <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
       <div className="description">
-        {item.descriptionFull}
+        {descriptionFull}
       </div>
       <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{item.currency} {item.price}</div>
+        <div className="price">{currency} {price}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
